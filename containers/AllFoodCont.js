7da421754711c83cee7b2604c9919bf7e59a3657
Iain <iain2eyes@gmail.com>
2020-05-02 05:15:54 +0100

    Set up basic file structure

---
+++ containers/AllFoodCont.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AllFoodCont = () => {
+  return <h2>All Food</h2>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AllFoodCont;
