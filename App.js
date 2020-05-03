298995f334347311a525efd030610f7b5705c329
Iain <iain2eyes@gmail.com>
2020-05-03 20:03:26 +0100

    Food item shows in To Get when selected

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AllFoodCont from "./containers/AllFoodCont";
 import ToGetCont from "./containers/ToGetCont";
 import InBasketCont from "./containers/InBasketCont";
 
 export default function App() {
-  let allFood = [
-    { name: "Apples", toGet: false, inBasket: false },
-    { name: "Tofu", toGet: true, inBasket: true },
-    { name: "Milk, Oat", toGet: true, inBasket: false },
-  ];
+  const [allFood, setAllFood] = useState([
+    { id: 1, name: "Apples", toGet: false, inBasket: false },
+    { id: 2, name: "Tofu", toGet: true, inBasket: true },
+    { id: 3, name: "Milk, Oat", toGet: true, inBasket: false },
+  ]);
 
   const toggleToGet = (food) => {
+    const allFoodCopy = [...allFood];
+    for (let i in allFoodCopy) {
+      if (allFoodCopy[i].name === food.name) {
+        allFoodCopy[i].toGet = !allFood[i].toGet;
+        setAllFood(allFoodCopy);
+      }
+    }
     console.log(food);
-    allFood[0].toGet = !allFood[0].toGet;
   };
 
   return (
